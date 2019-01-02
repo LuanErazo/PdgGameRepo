9866fbe0ed4906587f53902f2e4df62c771d0616
LuanErazo <luan.ep@outlook.com>
2019-01-02 18:41:46 -0500

    otra vez init comit

---
+++ Ui/Assets/Luan/Script/Pj.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pj : MonoBehaviour {
+
+
+    public LevelManagementData data;
+    public int idM;
+    public float speed;
+    public float speedX;
+
+
+    private bool Bmove;
+
+
+    private int mH, mV;
+
+    private void Awake()
+    {
+            
+    }
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        mH = (int)Input.GetAxisRaw("Horizontal");
+        mV = (int)Input.GetAxisRaw("Vertical");
+
+
+
+        if (Input.GetButtonDown("Horizontal") | Input.GetButtonDown("Vertical")) {
+            Bmove = true;
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("recolect"))
+        {
+            LevelManagementData.addPuntos(collision.GetComponent<recolectable>().puntaje);
+            data.recolectables--;
+            Destroy(collision.gameObject);
+        }
+    }
+
+    public int getmH() {
+        return mH;
+    }
+
+    public int getmV() {
+        return mV;
+    }
+
+    public bool getBmove() {
+        return Bmove;
+    }
+
+    public void setBmove(bool Bmove)
+    {
+        this.Bmove = Bmove;
+    }
+
+
+
+}
